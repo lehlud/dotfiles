6a3d9025a092b8d3e3e352b996d36eaa834f984c
Luddifee <ludwig@ludwig-lehnert.de>
2020-12-08 14:14:20 +0100

    general update

--- git/setup.sh
+++ git/setup.sh
@@ -1,8 +1,6 @@
 # install git
-sudo apt install git --assume-yes
+sudo pacman -S git --noconfirm
 
-# install keyring
-sudo apt install libsecret-1-0 libsecret-1-dev --assume-yes
-cd /usr/share/doc/git/contrib/credential/libsecret
-sudo make
-git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+# setup keyring
+sudo pacman -S libsecret --noconfirm
+git config --global credential.helper /usr/lib/git-core/git-credential-libsecret
