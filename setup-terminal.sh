64c1e572d2706af5b386cc6fc23ae5d477ffba57
Luddifee <ludwig@ludwig-lehnert.de>
2020-12-09 10:30:45 +0100

    general update

--- setup-terminal.sh
+++ setup-terminal.sh
@@ -5,4 +5,5 @@ sudo pacman -S curl --noconfirm
 sudo pacman -S alacritty --noconfirm
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp ~/.oh-my-zsh/custom/themes/example.zsh-theme ~/.oh-my-zsh/custom/themes/default.zsh-theme
+cp zshrc ~/.zshrc
 
