555ccc246417e35c76676edb1c20aabe75f94cd3
Luddifee <ludwig@ludwig-lehnert.de>
2020-12-09 10:49:07 +0100

    general update

---
+++ install-packages.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# haskell package
+sudo pacman -S haskell --noconfirm
+
+# python packages
+sudo pacman -S python3 pypy3 --noconfirm
+
+# js and ts packages
+sudo pacman -S deno nodejs --noconfirm
+
+# tex & markdown packages 
+sudo pacman -S texlive-core pandoc --noconfirm
+
