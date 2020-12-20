652f5fbe09abc5876e77ad3d551a4277f5640d53
Luddifee <ludwig@ludwig-lehnert.de>
2020-12-20 01:55:22 +0100

    added debian support (fix)

--- arch/vim-setup.sh
+++ arch/vim-setup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # install curl
 sudo pacman -S curl --noconfirm
 
@@ -12,11 +14,9 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # copy vimrc
-cp vimrc ~/.vimrc
+cp ../vimrc ~/.vimrc
 
 # source copied vimrc
 vim -c source ~/.vimrc -c qa!
 # install plugins
 vim -c PlugInstall vimtex -c coc.nvim -c PlugInstall lightline.vim -c PlugInstall nerdtree -c qa!
-#install coc plugins
-vim -c CocInstall coc-python -c qa!
