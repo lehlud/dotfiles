24aadaf1931b0f8ad15c86091f128b4bb16b0624
Luddifee <ludwig@ludwig-lehnert.de>
2021-01-09 20:42:42 +0100

    "npm--noconfirm" => "npm --noconfirm"

--- scripts/vim-setup.sh
+++ scripts/vim-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install vim, nodejs and npm (dependencies for coc.nvim)
-sudo pacman -S vim nodejs npm--noconfirm
+sudo pacman -S vim nodejs npm --noconfirm
 
 # setup vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
