0c150719a99dc4729d2fa178350833d9b32266e5
Luddifee <ludwig@ludwig-lehnert.de>
2021-01-09 13:19:01 +0100

    added bashrc

--- scripts/setup-laptop.sh
+++ scripts/setup-laptop.sh
@@ -12,7 +12,9 @@ sudo pacman -S kitty i3-gaps picom polybar nitrogen --noconfirm
 
 mkdir ~/.config/kitty ~/.config/i3 ~/.config/picom ~/.config/polybar
 
-cp .config/kitty/kitty.conf ~/.config/kitty/kitty.conf
-cp .config/i3/config-laptop ~/.config/i3/config
-cp .config/picom/picom.conf ~/.config/picom/picom.conf
-cp .config/polybar/config-laptop ~/.config/polybar/config
+cp ../.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+cp ../.config/i3/config-laptop ~/.config/i3/config
+cp ../.config/picom/picom.conf ~/.config/picom/picom.conf
+cp ../.config/polybar/config-laptop ~/.config/polybar/config
+
+cp ../.bashrc ~/.bashrc
