6858cdb4908ff65d53ba947762cd3c9a3def5d26
Ludwig Lehnert <info@ludwig-lenert.de>
2024-03-02 13:40:32 +0100

    sway status update

--- sway-status.sh
+++ sway-status.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
+if [[ $(pactl get-sink-mute "@DEFAULT_SINK@") == *"yes" ]]; then
+  volume="Mute"
+else
+  volume=$(pactl get-sink-volume "@DEFAULT_SINK@" | grep -Po '\d+%' | head -n 1)
+fi
+
+mem=$(free --mega | tail -2 | awk '{print s+=$3}' | tail -1 | awk '{printf "%.2f GB", $1/1000}')
+
+cpu=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf "%.2f%", usage}')
+
 bat_level=$(cat /sys/class/power_supply/BAT*/capacity)
 bat_state=$(cat /sys/class/power_supply/BAT*/status)
 
 date=$(date +'%d.%m.%Y %H:%M:%S')
 
-echo "$bat_level%" $bat_state "|" $date ""
+status="Audio $volume | CPU $cpu | RAM $mem | $bat_level% $bat_state | $date"
+
+wifi=$(nmcli -t -f name,type connection show --active | grep wireless | cut -d\: -f1)
+if [[ -n $wifi ]]; then
+  status="$wifi | $status"
+fi
+
+echo $status
 
