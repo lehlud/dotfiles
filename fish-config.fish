85a5ddf4c46f18a85a191dc121e9a89234aa562b
Ludwig Lehnert <mail@ludwig-lehnert.de>
2024-03-18 21:20:29 +0100

    small changes

--- fish-config.fish
+++ fish-config.fish
@@ -4,10 +4,6 @@ if status is-interactive && type -q pfetch
     pfetch
 end
 
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/ludwig/.ghcup/bin # ghcup-env
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
-
+set -x EDITOR hx
+alias tmux="tmux -2"
 
