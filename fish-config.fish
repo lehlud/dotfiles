4d882968f977a25f7befe5d11d2bee321164dca3
Ludwig Lehnert <mail@ludwig-lehnert.de>
2024-04-08 18:13:45 +0200

    added json shortcuts to fish

--- fish-config.fish
+++ fish-config.fish
@@ -7,3 +7,6 @@ end
 set -x EDITOR hx
 alias tmux="tmux -2"
 
+alias json-get="curl --request GET -H 'Content-Type: application/json'"
+alias json-post="curl --request POST -H 'Content-Type: application/json'"
+
