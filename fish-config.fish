a0d375f523e893e9017f25ef1fe54615fbdea0a6
Ludwig Lehnert <github@ludwig-lehnert.de>
2024-10-01 15:36:16 +0200

    update fish config

--- fish-config.fish
+++ fish-config.fish
@@ -1,7 +1,5 @@
-if status is-interactive && type -q pfetch
+if status is-interactive
     # Commands to run in interactive sessions can go here
-    echo
-    pfetch
 end
 
 set -x EDITOR hx
@@ -9,4 +7,3 @@ alias tmux="tmux -2"
 
 alias json-get="curl --request GET -H 'Content-Type: application/json'"
 alias json-post="curl --request POST -H 'Content-Type: application/json'"
-
